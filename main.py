f327cde22114cc5cdfb9fc4b10a771aeb4b6dda7
woodsjc <woodsjc0@gmail.com>
2016-12-21 12:24:17 -0600

    added s&p500 downloader

--- main.py
+++ main.py
@@ -5,14 +5,18 @@ import os
 import pandas as pd
 import numpy as np
 
+#wating on pull request to work with python 3 for now can install from https://github.com/woodsjc/finsymbols
+import finsymbols
+
 START = datetime(2016,11,1)
-END = datetime(2016,12,20)
+END = datetime.today()
 
 def get_data(name='HPE'):
     data = pdr.DataReader(name, 'yahoo', START, END)
     if not os.path.isdir('./data'):
         os.mkdir('./data')
     data.to_csv('./data/' + name + '.csv')
+    print("Wrote ./data" + name + '.csv')
     data.describe()
     return data
 
@@ -39,6 +43,32 @@ def moving_avg_window(d, timescale=5):
         start += 1 
     return pd.DataFrame(data=tmp, index=d[start:].index, columns=["moving average: " + d.name])
 
+def dl_sp500_data():
+    sp500 = finsymbols.get_sp500_symbols()
+    data = []
+
+    for s in sp500:
+        if 'symbol' in s:
+            data.append(get_data(s['symbol']))
+
+    return data
+
+def load_sp500_data():
+    sp500 = finsymbols.get_sp500_symbols()
+    data = []
+
+    for s in sp500:
+        if 'symbol' in s:
+            symbol = s['symbol']
+            tmp = load_data(symbol)
+            if tmp is None:
+                data.append(get_data(symbol))
+            else:
+                data.append(tmp)
+
+    return data
+
+
 stock_name = 'hpe'
 
 #data = get_data(stock_name)
@@ -60,3 +90,9 @@ plt.close()
 plt.plot(moving_avg_window(data.ix[-10:]['Close']))
 plt.pause(.001)
 plt.pause(.001)
+plt.close()
+plt.pause(.001)
+
+#sp500 = dl_sp500_data()
+sp500 = load_sp500_data()
+
